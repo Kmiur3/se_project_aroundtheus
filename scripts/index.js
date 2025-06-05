8a2a0a0a14a0661fbe52467b0a3c639337d0077f
Kmiur3 <Kierra_bartlett@yahoo.com>
2025-06-05 15:31:15 -0500

    linked JavaScript and added titles with URLs

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,28 @@
-/* const initialCards = ["name", "link"];
+const initialCards = [
+  {
+    title: "Stonehenge",
+    link: "https://drive.google.com/file/d/1sfX1qAp-g3A9gLQ8KAMQRBL6kz2MZBFt/view?usp=sharing",
+  },
+  {
+    title: "Big Ben",
+    link: "https://drive.google.com/file/d/1thX6qR_qbA-49cJdV4gasqp8jOzxMBfO/view?usp=sharing",
+  },
+  {
+    title: "Cliffs of Moher",
+    link: "https://drive.google.com/file/d/1od8EBagWlUMLSV_ynZWepQadNVIpDT37/view?usp=sharing",
+  },
+  {
+    title: "Mont St Michel",
+    link: "https://drive.google.com/file/d/1q-X48WhjBa-Zngha9PV6-tiLIrPAtBmf/view?usp=sharing",
+  },
+  {
+    title: "Matterhorn",
+    link: "https://drive.google.com/file/d/1Jj0Ynl9WJ_8BD3OAMsl4aic8LSsjqC8w/view?usp=sharing",
+  },
+  {
+    title: "Kirkjufell",
+    link: "https://drive.google.com/file/d/16vl3J2WY60-_IoYKC50kGh5kXtDWTiVY/view?usp=sharing",
+  },
+];
 
-const cardData = {
-  title: "Stonehenge",
-};
-
-console.log(cardData); */
+console.log(initialCards);
